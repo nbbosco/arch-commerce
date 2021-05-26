4485244b6328e0078f78a1ae901f97965ca4c3e0
nbbosco <nickilu009@gmail.com>
2021-05-26 14:28:23 -0300

    NB_Archivos

--- app.js
+++ app.js
@@ -10,6 +10,23 @@ app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname,"./views/login.html"));
+app.get("/", (_req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/home.html"));
 })
+
+app.get("/carrito", (_req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/carrito.html"));
+})
+
+app.get('/login', (_req, res) => {
+    let htmlPath = path.join(__dirname, './views/login.html')
+    res.sendFile(htmlPath);
+})
+
+app.get("/producto", (_req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/producto.html"));
+})
+
+app.get("/registro", (_req, res) => {
+    res.sendFile(path.resolve(__dirname,"./views/registro.html"));
+});
