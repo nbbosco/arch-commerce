c7dc57ddc815758f070434d61feb8a46d908d692
nbbosco <nickilu009@gmail.com>
2021-06-20 18:13:54 -0300

    NB_Sprint3

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,7 +2,9 @@ const productController = {
     carrito: (req, res) => {
         res.render('./products/carrito')},
     producto: (req, res) => {
-        res.render('./products/producto')}
+        res.render('./products/producto')},
+    crear: (req, res) => {
+        res.render('./products/product-edit')},
 }
 
 module.exports = productController;
--- src/routes/products.js
+++ src/routes/products.js
@@ -5,5 +5,6 @@ const routes = express.Router();
 
 routes.get('/carrito',productsController.carrito)
 routes.get('/producto',productsController.producto)
+routes.get('/producto/crear',productsController.crear)
 
 module.exports = routes;
