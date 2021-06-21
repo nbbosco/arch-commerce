a979f0e9157b92da2dbf57d75ce5cbf680276c0e
nbbosco <nickilu009@gmail.com>
2021-06-20 21:48:38 -0300

    NB_Sprint3

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.get('/carrito',productsController.carrito)
 routes.get('/producto',productsController.producto)
 routes.get('/producto/crear',productsController.crear)
+routes.get('/producto/editar',productsController.editar)
 
 module.exports = routes;
