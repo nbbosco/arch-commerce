ffe0b9dac62d897a1c8ff449d6bf3d5f30c95305
nbbosco <nickilu009@gmail.com>
2021-07-02 16:59:55 -0300

    NB_Heroku

--- src/app.js
+++ src/app.js
@@ -11,14 +11,16 @@ const publicPath = path.resolve(__dirname, "../public");
 app.use( express.static(publicPath));
 
 app.set ('view engine', 'ejs');
+app.use('/', mainRoutes, productsRoutes, usersRoutes);
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("Servidor corriendo en el puerto 3000");
 });
+
 
 //Localhost
 // app.listen(3000, () => {
 //     console.log("Servidor corriendo");
 // });
 
-app.use('/', mainRoutes, productsRoutes, usersRoutes);
+
