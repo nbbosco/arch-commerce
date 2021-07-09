a0d04d5db118ed1d3a6e67f0d982fc0894399348
nbbosco <nickilu009@gmail.com>
2021-07-09 13:58:15 -0300

    NB_Sprint 4

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -26,16 +26,67 @@ const productController = {
     },
 
     store: (req, res) => {
+        let nombreImagen = req.file.filename;
         let idNuevo = products[products.length-1].id + 1;
-        let nuevoObjeto = Object.assign({id: idNuevo},req.body);
+        let nuevoObjeto = Object.assign({id: idNuevo},req.body,{imagen:nombreImagen});
         products.push(nuevoObjeto);
-        fs.writeFileSync(productsFilePath, JSON.stringify(products,null));
+        fs.writeFileSync(productsFilePath, JSON.stringify(products,null, ''));
         res.redirect('/');
     },
 
     editar: (req, res) => {
-        res.render('./products/editar')
+        let idProducto= req.params.id
+		for(let i=0;i<products.length;i++){
+			if (products[i].id==idProducto){
+				var productoEncontrado = products[i];
+			}
+		}
+        res.render('./products/editar', {productoEnDetalle: productoEncontrado});
     },
+
+    update: (req, res) => {
+		let valoresNuevos = req.body;
+		let idProducto= req.params.id
+
+		let nombreImagen = req.file.filename
+
+		for(let i=0;i<products.length;i++){
+			if (products[i].id==idProducto){
+				
+				var imagenAnterior = products[i].imagen
+				products[i].nombreProducto = valoresNuevos.nombreProducto;
+				products[i].precio = valoresNuevos.precio;
+                products[i].fechaDeCreacion = valoresNuevos.fechaDeCreacion;
+                products[i].creador = valoresNuevos.creador;
+                products[i].coleccionista = valoresNuevos.coleccionista;
+				products[i].categoria = valoresNuevos.categoria;
+				products[i].descripcion = valoresNuevos.descripcion;
+				products[i].imagen = nombreImagen;
+
+				var productoEncontrado = products[i];
+
+				break;
+			}
+		}
+
+		fs.writeFileSync(productsFilePath, JSON.stringify(products,null, ''));
+		fs.unlinkSync(path.join(__dirname,'../../public/img' + imagenAnterior));
+		res.render('productoDetalle', {productoEnDetalle: productoEncontrado});
+	},
+
+    destroy : (req, res) => {
+		let idProducto= req.params.id
+		for(let i=0;i<products.length;i++){
+			if (products[i].id==idProducto){}
+				var nombreImagen = products[i].imagen;
+				products.splice(i,1);
+				break;
+			}
+			
+			fs.writeFileSync(productsFilePath, JSON.stringify(products,null, ''));
+			fs.unlinkSync(path.join(__dirname,'../../public/img' + nombreImagen));
+			res.render('home', {productos: products});
+		},
 
     carrito: (req, res) => {
         res.render('./products/carrito', {productos: products})
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,19 +2,34 @@ const productsController = require('../controllers/productController');
 
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+let multerDiskStorage = multer.diskStorage({
+	destination: function(req, file, callback){
+        let folder = path.join(__dirname, "../../public/img");
+        callback(null, folder)
+},
+	filename: function(req, file, callback){
+		let imageName = 'img' + '-' + file.originalname;
+		callback(null, imageName);
+}
+});
+
+let uploadFile = multer({storage: multerDiskStorage});
 
 router.get('/', productsController.producto); 
 router.get('/carrito', productsController.carrito)
 
 router.get('/create', productsController.crear); 
-router.post('/create', productsController.store); 
+router.post('/create', uploadFile.single('imagen'), productsController.store); 
 
 router.get('/:id', productsController.detalle); 
 
 router.get('/:id/edit', productsController.editar); 
-// router.put('/:id', productsController.update); 
+router.put('/:id/edit', productsController.update); 
 
-// router.delete('/:id', productsController.eliminar); 
+router.delete('/:id', productsController.destroy); 
 
 
 module.exports = router;
