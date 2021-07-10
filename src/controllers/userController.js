0599ca615a0006d3930c1a0703c4d82927e73d96
nbbosco <nickilu009@gmail.com>
2021-07-10 15:21:37 -0300

    NB_Sprint 4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../database/userDataBase.json');
+const productsFilePath = path.join(__dirname, '../database/userDatabase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
