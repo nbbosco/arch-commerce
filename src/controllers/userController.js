8bd6c53ff1b02de87032f74b84214dc3e241e3b9
milagros-hamwee <milihamwee@gmail.com>
2021-07-24 20:57:53 -0300

    MH_EditarUsuarios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const productsFilePath = path.join(__dirname, '../database/userDatabase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, '../database/userDatabase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -20,6 +20,38 @@ const userController = {
         fs.writeFileSync(productsFilePath, JSON.stringify(products,null, 4));
         res.redirect('/');
     },
+    editar: (req, res) => {
+        res.render('./users/editar')
+    },
+    update: (req, res) => {
+		let valoresNuevos = req.body;
+		let idUser= req.params.id
+
+		let nombreImagen = req.file.filename
+
+		for(let i=0;i<users.length;i++){
+			if (products[i].id==idUser){
+				
+				var imagenAnterior = users[i].imagen
+				users[i].nombre = valoresNuevos.nombre;
+                users[i].apellido = valoresNuevos.apellido;
+                users[i].nombre = valoresNuevos.nombre;
+                users[i].email = valoresNuevos.email;
+                users[i].telefono = valoresNuevos.telefono;
+                users[i].fechaNacimiento = valoresNuevos.fechaNacimiento;
+                users[i].creador = valoresNuevos.creador;
+                users[i].coleccionista = valoresNuevos.coleccionista;
+
+				var userEncontrado = users[i];
+
+				break;
+			}
+		}
+
+		fs.writeFileSync(usersFilePath, JSON.stringify(users,null,4));
+		fs.unlinkSync(path.join(__dirname,'../../public/img/' + imagenAnterior));
+		res.render('./users/editar', {users: userEncontrado});
+	},
 }
 
 module.exports = userController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,8 @@ const router = express.Router();
 
 router.get('/login',userController.login)
 router.get('/registro',userController.registro)
-router.post('/registro', userController.store); 
+router.post('/registro', userController.store);
+router.get ('/editar', userController.editar);
+router.get ('/editar', userController.update);
 
 module.exports = router;
